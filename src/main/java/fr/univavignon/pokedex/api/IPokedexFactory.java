60094516df1487452f511e662b33250c82bb4049
amos <amosdorceus2010@gmail.com>
2022-05-12 09:36:14 +0200

    Test generation java doc

--- src/main/java/fr/univavignon/pokedex/api/IPokedexFactory.java
+++ src/main/java/fr/univavignon/pokedex/api/IPokedexFactory.java
@@ -3,7 +3,7 @@ package fr.univavignon.pokedex.api;
 /**
  * Factory interface for class that aims to create IPokedex instance.
  * 
- * @author fv
+ * @author fv c'est Amos
  */
 public interface IPokedexFactory {
 
